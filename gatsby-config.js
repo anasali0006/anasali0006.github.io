0e1d18597bbd2641ccda03fb8f2e69b7527d9883
Anas Ali <anasali0006@gmail.com>
2024-02-08 20:24:41 +0500

    updating names in some places

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Anas Ali',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Anas Ali is a Data Scientist and a Python Developer, having expertise in designing solutions using ML, AI, GenerativeAI and NLP',
+    siteUrl: 'https://anasali0006.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
